e50bb6b76ba9f18626894cf7ff6fabfc83a95fa5
Jenny Berggreen <143997461+jenny-berggreen@users.noreply.github.com>
2023-10-23 18:44:52 +0200

    Toggle switch hover classes based on switch state

--- js/index.js
+++ js/index.js
@@ -46,8 +46,36 @@ accordionButton.addEventListener('click', displayAccordionContent);
 const switchComponent = document.querySelector('.component-state--interactive .switch__container');
 
 function toggleSwitchAndSlider() {
-	switchComponent.classList.toggle('switch--on');
-	switchComponent.classList.toggle('switch__slider--on');
+    switchComponent.classList.toggle('switch--on');
+    switchComponent.classList.toggle('switch__slider--on');
+
+    // After clicking, display correct hover state based on switch state
+    if (switchComponent.classList.contains('switch--on')) {		// switch is on:
+        switchComponent.classList.remove('switch--off-hover');	// remove off hover class
+        switchComponent.classList.add('switch--on-hover');		// display on hover color
+    } 
+	else {														// switch is off:
+        switchComponent.classList.remove('switch--on-hover');	// remove on hover class
+        switchComponent.classList.add('switch--off-hover');		// display off hover color
+    }
 }
 
+function addSwitchHoverClass() {
+    if (!switchComponent.classList.contains('switch--on')) { 	// switch is off:
+        switchComponent.classList.add('switch--off-hover'); 	// display off hover color
+    } 
+	else { 														// switch is on:
+        switchComponent.classList.add('switch--on-hover'); 		// display on hover color
+    }
+}
+
+// remove all hover classes, display only off or on state
+function removeSwitchHoverClass() {
+    switchComponent.classList.remove('switch--on-hover');
+    switchComponent.classList.remove('switch--off-hover');
+}
+
+switchComponent.addEventListener('mouseover', addSwitchHoverClass);
+switchComponent.addEventListener('mouseleave', removeSwitchHoverClass);
 switchComponent.addEventListener('click', toggleSwitchAndSlider);
+
